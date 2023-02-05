2121006de8259cbd083c3c2a946abdc95a7f2311
Adam Romero <aromero0904@gmail.com>
2023-02-05 18:52:57 -0500

    commit tree eaab2cb8eef97f19a5b44a964f9367565de34913

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,28 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+
+
+    def initialize(name)
+        @name=name
+        def bank_account(balance=25)
+            @balance=balance
+        end
+        def happiness(happy=8)
+            @happy=happy
+        end
+    end
+
+    def bank_account=(new_balance)
+        @balance=new_balance
+    end
+
+    def happiness=(happy)
+       @happy = happy
+    end
+
+    def happiness
+        @happy
+    end
+end
